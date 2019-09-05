c4260501b34540b6f772bc3defbf7ee9232a5893
Victor Cheng <victorcheng23@gmail.com>
2019-09-05 16:30:51 -0400

    linkedlist loop complete

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,11 +1,22 @@
-'use strict';
+"use strict";
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = linkedlist => {
+  const container = {};
+  let result = false;
 
+  let currentNode = linkedlist.head;
+
+  while (currentNode) {
+    if (container[currentNode.value]) {
+      result = true;
+      break;
+    }
+    container[currentNode.value] = currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return result;
 };
-
-
 /*
 EXTRA CREDIT:
 
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
